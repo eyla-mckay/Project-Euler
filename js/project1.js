8201a012d12d3235b28a341648274b5930e2bcd5
eyla-mckay <109965971+eyla-mckay@users.noreply.github.com>
2023-09-16 17:18:54 +0200

    simplify P1 algorithm

--- js/project1.js
+++ js/project1.js
@@ -1,56 +1,27 @@
 // If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
 // Find the sum of all the multiples of 3 or 5 below 1000.
 
-
-/* Function: create array of natural numbers up to N */
-function generate_numbers(N) {
-    let temp_result = [];
-    for (let i=1; i<N; i++) {
-        temp_result.push(i);
-    }
-    return temp_result;
-}
-
-/* Function: test if q is a factor of p */
-function is_multiple(p,q) {
-    if ((p%q) == 0) {
-        return true;
-    } else {
-        return false;
-    }
-}
-
-/* Function: retrieve multiples of q1 or q2 from array */
-function extract_multiples(array,quotient1,quotient2) {
-    let temp_result = [];
-    for (let i of array) {
-        if (is_multiple(i,quotient1) || is_multiple(i,quotient2)) {
-            temp_result.push(i);
-        }
-    }
-    return temp_result;
-}
-
-/* Function: sum all elements */
-function summing(elements) {
-    let sum = 0;
-    for (let i of elements) {
-        sum += i;
-    }
-    return sum;
-}
-
-/* Function: calculate and write answer */
-function execute(N,q1,q2) {
-    const result = document.getElementById("result");
-    let multiples_list = extract_multiples(generate_numbers(N),q1,q2);
-    result.innerHTML=`The natural numbers below ${N} that are multiples of ${q1} or ${q2} are: ${multiples_list}. <br/>Their sum is: ${summing(multiples_list)}`;
-}
-
-/* Function: main process */
 function main() {
-    let q1 = parseInt(document.getElementById("q1").value);
-    let q2 = parseInt(document.getElementById("q2").value);
-    let n = parseInt(document.getElementById("n").value);
-    execute(n,q1,q2);
+
+    /* acquire variables */
+    const n  = parseInt(document.getElementById("n").value);
+    const q1 = parseInt(document.getElementById("q1").value);
+    const q2 = parseInt(document.getElementById("q2").value);
+
+    /* calculate */
+
+    let sum = 0;
+    // let multiples = []; // initialise list (if we want the list of multiples for later)
+
+    for (let i=1; i<n; i++) { // for each natural number until N
+
+        if ( ((i%q1) == 0) || ((i%q2) == 0)) { // if evenly divisible by Q1 or Q2
+
+            sum += i;
+            // multiples.push(i) // save to array (if we want the list of multiples for later)
+            }
+        }
+    
+    /* write answer */
+    document.getElementById("result").innerHTML = `The sum of natural numbers below ${n} that are multiples of ${q1} or ${q2} is: ${sum}`;
 }
